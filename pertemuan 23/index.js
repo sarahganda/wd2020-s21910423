fbc5820f3f2971c585025c77a8f26e99ee0b6aa7
sarahganda <s21910423@student.unklab.ac.id>
2020-11-09 11:48:43 +0700

    js

---
+++ pertemuan 23/index.js
@@ -0,0 +1,9 @@
+/// berat/tinggi*tinggi
+let berat = prompt("masukan berat =");
+console.log("berat yang anda masukkan adalah" + berat);
+let tinggi = prompt("masukan tinggi =");
+console.log("tinggi yang anda masukkan adalah" + tinggi);
+
+let bilangan1=berat;
+let bilangan2=tinggi;
+console.log(berat / tinggi * tinggi);
